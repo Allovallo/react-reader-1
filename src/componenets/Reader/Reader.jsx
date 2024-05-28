e10b5c8f3c1ab1056ce621519d9b9548d289842c
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-05-28 18:31:11 +0300

    files updated

---
+++ src/componenets/Reader/Reader.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export class Reader extends Component {
+  state = {
+    publicationIndex: 0,
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./componenets/GlobalStyle";
+import { Reader } from "./componenets/Reader/Reader";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <GlobalStyle />
+      <Reader />
+    </ThemeProvider>
+  </React.StrictMode>
+);
